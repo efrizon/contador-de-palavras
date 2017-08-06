c08954e81f32dac2144961c67d3c501327b0400a
efrizon <30643012+efrizon@users.noreply.github.com>
2017-08-06 16:34:58 -0300

    Projeto finalizado

--- src/main.c
+++ src/main.c
@@ -9,10 +9,10 @@ int main() {
 	while (c != '\n') {
 		scanf("%c", &c);
 
-		if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')   C = 0 , D = 1;
-		else if (c == '.' || c == ',')                      C = 1 , D = 1;
-		else if (c >= '0' && c <= '9')                      C = 1 , D = 0;
-		else                                                C = 0 , D = 0;
+		if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z') C = 0 , D = 1;
+		else if (c == '.' || c == ',') C = 1 , D = 1;
+		else if (c >= '0' && c <= '9') C = 1 , D = 0;
+		else C = 0 , D = 0;
 
 		if (!A && !B && !C && D || !A && !B && C && !D || A && B && !C && D) n++;
 		
