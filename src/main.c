f464d80c1cce0feb2c84c75e875b2cce134c3128
efrizon <30643012+efrizon@users.noreply.github.com>
2017-08-06 19:18:37 +0000

    Projeto finalizado

--- src/main.c
+++ src/main.c
@@ -4,18 +4,18 @@ int main() {
 	int A , B , C , D , n;
 	char c;
 
-    A = B = n = 0;
+	A = B = n = 0;
 
 	while (c != '\n') {
 		scanf("%c", &c);
 
-        if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')   C = 0 , D = 1;
-        else if (c == '.' || c == ',')                      C = 1 , D = 1;
-        else if (c >= '0' && c <= '9')                      C = 1 , D = 0;
-        else                                                C = 0 , D = 0;
+		if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')   C = 0 , D = 1;
+		else if (c == '.' || c == ',')                      C = 1 , D = 1;
+		else if (c >= '0' && c <= '9')                      C = 1 , D = 0;
+		else                                                C = 0 , D = 0;
 
-	if (!A && !B && !C && D || !A && !B && C && !D || A && B && !C && D) n++;
-
+		if (!A && !B && !C && D || !A && !B && C && !D || A && B && !C && D) n++;
+		
 		A = C , B = D;
 	}
 	printf ("%d\n", n);
