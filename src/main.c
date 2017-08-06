455b66605ca50bddbbfdfafe207e52246899c303
efrizon <30643012+efrizon@users.noreply.github.com>
2017-08-06 15:38:41 +0000

    O programa está corrigido.
    Ajuste da identação

--- src/main.c
+++ src/main.c
@@ -1,8 +1,7 @@
 /* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
+Este programa recebera uma serie de caracteres representando palavras em sua
+entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
+numero de palavras separadas que recebeu e, apos, encerrar.
  */
 
 #include <stdio.h>
