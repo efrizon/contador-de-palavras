d9791302136266b1b58e834647b76f57b4f109d2
efrizon <30643012+efrizon@users.noreply.github.com>
2017-08-06 15:42:07 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -18,15 +18,15 @@ int main() {
 
 		scanf("%c", &c);
 
-   		if (b >= 'A' && b <= 'Z' || b >= 'a' && b <= 'z')	A = 0 , B = 1;
-		else if (b == '.' || b == ',')						A = 1 , B = 1;
-		else if (b >= '0' && b <= '9')						A = 1 , B = 0;
-		else												A = 0 , B = 0;
+   		if (b >= 'A' && b <= 'Z' || b >= 'a' && b <= 'z')   A = 0 , B = 1;
+		else if (b == '.' || b == ',')                      A = 1 , B = 1;
+		else if (b >= '0' && b <= '9')                      A = 1 , B = 0;
+		else                                                A = 0 , B = 0;
 
-		if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')	C = 0 , D = 1;
-		else if (c == '.' || c == ',')						C = 1 , D = 1;
-		else if (c >= '0' && c <= '9')						C = 1 , D = 0;
-		else												C = 0 , D = 0;
+		if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')   C = 0 , D = 1;
+		else if (c == '.' || c == ',')                      C = 1 , D = 1;
+		else if (c >= '0' && c <= '9')                      C = 1 , D = 0;
+		else                                                C = 0 , D = 0;
 
 		if ((!A && !B && !C && D)||(!A && !B && C && !D)||(A && B && !C && D)) n++;
 		
